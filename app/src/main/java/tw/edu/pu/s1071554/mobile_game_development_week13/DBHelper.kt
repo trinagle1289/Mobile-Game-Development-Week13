f46b8da121686bbea7f699361e509cef54233395
trinagle1289 <denny95090@gmail.com>
2022-05-21 17:04:59 +0800

    [feat]Finish Database

---
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_week13/DBHelper.kt
@@ -0,0 +1,52 @@
+package tw.edu.pu.s1071554.mobile_game_development_week13
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+data class UserModel(val name: String, val age: String)
+
+class DBHelper(
+    context: Context?,
+) : SQLiteOpenHelper(context, "mydb", null, 1) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        // TODO("Not yet implemented")
+        p0?.execSQL(" create table if not exists usermodel(id integer PRIMARY KEY autoincrement, name text, age text)")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        //TODO("Not yet implemented")
+        p0?.execSQL("drop table usermodel")
+        onCreate(p0)
+    }
+
+    fun showAllUser(): ArrayList<UserModel> {
+        var db = writableDatabase
+        var users = ArrayList<UserModel>()
+        var c1 = db.rawQuery("select * from usermodel", null)
+        while (c1.moveToNext()) {
+            var nameCol = c1.getColumnIndex("name")
+            var name = c1.getString(nameCol)
+            var ageCol = c1.getColumnIndex("age")
+            var age = c1.getString(ageCol)
+            var user = UserModel(name, age)
+            users.add(user)
+        }
+        return users
+    }
+
+    fun delUser(name: String) {
+        var db = writableDatabase
+        var selstr = "name LIKE '$name%'"
+        db.delete("usermodel", selstr, arrayOf())
+    }
+
+    fun insertUser(user: UserModel) {
+        var db = writableDatabase
+
+        var sqlstr: String =
+            "insert into usermodel(name, age) values ('" + user.name + "','" + user.age + "' )"
+        db.execSQL(sqlstr)
+    }
+
+}
--- app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_week13/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1071554/mobile_game_development_week13/MainActivity.kt
@@ -1,6 +1,8 @@
 package tw.edu.pu.s1071554.mobile_game_development_week13
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,11 +10,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.s1071554.mobile_game_development_week13.ui.theme.MobileGameDevelopmentWeek13Theme
+import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
+    //lateinit var db: SQLiteDatabase
+    var dbHelper: DBHelper = DBHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,12 +32,31 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        try {
+            var user1 = UserModel("ToA", "50")
+            dbHelper.insertUser(user1)
+            var user2 = UserModel("ToB", "50")
+            dbHelper.insertUser(user2)
+            //dbHelper.delUser("To")
+
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_LONG).show()
+        }
+        var users = dbHelper.showAllUser()
+        var tstr = ""
+        for (user in users) {
+            tstr += user.name + "," + user.age + "\n"
+        }
+        totalStr.value = tstr
+        // Toast.makeText(applicationContext , totalText, Toast.LENGTH_LONG).show()
     }
 }
 
+var totalStr = mutableStateOf("")
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello ${totalStr.value}!")
 }
 
 @Preview(showBackground = true)
